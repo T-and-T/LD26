5bcc263040a0845c907668ee0d64c572672cfa84
Nathan van Doorn <nvd1234@gmail.com>
2013-04-28 15:08:26 +0100

    Inlined and removed stolen code.

--- js/math_utils.js
+++ js/math_utils.js
@@ -11,16 +11,11 @@ function choose(n, k){
     return (fact(n)/(fact(k)*fact(n-k)));
 }
 
-function getRandomInt(min, max) {
-  // stolen from MDN
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function bin1_2(n){
     //chooses a random number between 0 and n
     //according to a binomial distribution
     //with p = 0.5
-    var i = getRandomInt(0, Math.pow(2,n-1));
+    var i = Math.floor(Math.random() * (Math.pow(2,n-1) + 1));
     var j = 0;
     var r = 0;
     while (i > 0 && j <= n-1) {
