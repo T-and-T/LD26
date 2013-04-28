423c990b30f4ac2a71d333e381d8d20dea21db36
Nathan van Doorn <nvd1234@gmail.com>
2013-04-28 18:39:49 +0100

    Added AI movement. Still need to do: * die * fall of screen * eat * be different colours

--- js/aientity.js
+++ js/aientity.js
@@ -1,7 +1,16 @@
-function AIEntity() {}
+function AIEntity(game, level, direction) {
+    this.game = game;
+    this.timer = 0;
+    this.level = level;
+    this.time = level * 100;
+    this.direction = direction;
+    this.moving = direction;
+}
 
 AIEntity.prototype = new Entity(1); // inherit from Entity and set level to 1
 
-AIEntity.prototype.update = function() {}
+AIEntity.prototype.update = function() {
+    this.moving = this.direction;
+}
 
 AIEntity.prototype.die = function() {}
